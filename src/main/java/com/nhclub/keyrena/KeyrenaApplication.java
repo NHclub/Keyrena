46b6ed871ad5f5afdf5f5eb00e2532d44929ca37
NHclub <sbe4488@naver.com>
2023-11-15 18:22:39 +0900

    init commit

---
+++ src/main/java/com/nhclub/keyrena/KeyrenaApplication.java
@@ -0,0 +1,13 @@
+package com.nhclub.keyrena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyrenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyrenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhclub/keyrena/KeyrenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhclub.keyrena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyrenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
